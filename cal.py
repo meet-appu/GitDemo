80b8db3b5adfaac8423affbdcf9a1d3b9ba3712c
Meet <124564308+meet-appu@users.noreply.github.com>
2023-02-24 11:57:16 +0530

    changes in calcultor

--- cal.py
+++ cal.py
@@ -1,19 +1,30 @@
-a=15
-b=25
-c=a+b
-print ("sum is",c)
+print("Select an operation to perform: ")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
 
+operation = input()
 
-a=25
-b=5
-c=a/b
-print("division is",c)
+if operation == "1":
+    num1 = input("Enter first no.:")
+    num2 = input("Enter second no.:")
+    print("The sum is="+ str(int(num1)+ int(num2)))
 
-a=25
-b=5
-c=a*b
-print("multiplication is",c)
-a=25
-b=5
-c=a-b
-print("Substraction is",c)
+elif operation == "2":
+    num1 = input("Enter first no.:")
+    num2 = input("Enter second no.:")
+    print("The substraction is="+ str(int(num1) - int(num2)))
+    
+elif operation == "3":
+    num1 = input("Enter first no.:")
+    num2 = input("Enter second no.:")
+    print("The multiplication is="+ str(int(num1) * int(num2)))
+
+elif operation == "4":
+    num1 = input("Enter first no.:")
+    num2 = input("Enter second no.:")
+    print("The division is="+ str(int(num1)/int( num2)))    
+
+else:
+    print ("invalid entry")    
